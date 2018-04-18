926fe8e09ae76441bd3da2b2fdb2c7c9e79dc8c6
Stefan Plavsic <stef.plav@gmail.com>
2018-04-18 21:54:10 +0200

    dont touch

--- src/ObjectWriter.java
+++ src/ObjectWriter.java
@@ -1,7 +1,9 @@
+
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class ObjectWriter {
@@ -11,15 +13,48 @@ public class ObjectWriter {
 		PrintWriter writer = new PrintWriter(new File("C:\\Users\\Stefan\\Desktop\\test.txt"));
 
 		Method[] m = obj.getClass().getMethods();
+		
+		PrintWriter w = new PrintWriter(out);
+		getPrimitveTypes(obj, w);
+		
+	/*	
+		try (PrintWriter w = new PrintWriter(out)) { 
+			
 
 		for (int i = 0; i < m.length; i++) {
 			Method method = m[i];
 			System.out.println(m[i]);
 			writer.println(method.getName());
 		}
-
+		
+		}
+*/
 		writer.flush();
 		writer.close();
 
 	}
+	
+	public void getPrimitveTypes(Object obj,PrintWriter w){
+		
+		Class<?>clazz=obj.getClass();
+		
+		Field[]fields=clazz.getDeclaredFields();
+		
+		for (int i = 0; i < fields.length; i++) {
+			
+			Field field = fields[i];
+			if (field.getModifiers()==1) 
+			{
+				System.out.println(field.getName());
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 
+import serial.ObjectWriter;
+
 public class main {
 
 	public static void main(String[] args) throws IOException {
